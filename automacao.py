fa6d53f5546687183d1854017a6d8576044a2a07
LUIZ GUILHERME LEITE SIQUEIRA DA COSTA <luiz.leite@viavarejo.com.br>
2025-03-13 16:01:59 -0300

    Automação do reasoncode está passando por todas as telas e opções, refinar o código

--- automacao.py
+++ automacao.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 import pandas as pd
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 from time import sleep
 
 class AutoClick:
@@ -24,12 +26,26 @@ class AutoClick:
     def pressionar_enter(self, xpath: str, timeout: int = 30):
         elemento = self._encontrar_elemento(xpath, timeout, EC.element_to_be_clickable)
         elemento.send_keys(Keys.RETURN)
+
+    def clear_field(self, xpath: str, timeout: int = 30):
+        elemento = self._encontrar_elemento(xpath, timeout, EC.element_to_be_clickable)
+        elemento.clear()
     
     def selecionar(self, xpath: str, timeout: int = 30):
         elemento = WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, xpath)))
         print("Elemento encontrado:", elemento.text)
         return elemento.text
 
+    def scroll_to_element(self, xpath):
+        elemento = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, xpath))
+        )
+        
+        # Executa scroll até o elemento usando JavaScript
+        self.driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", elemento)
+        # sleep(1)
+
+        return elemento
     
 
 class Automation():
@@ -70,31 +86,94 @@ class Automation():
             except Exception as e:
                 print(f"Elemento não foi encontrado ou não ficou visivel{e}")    
 
+    def executar_script(self, script: str, texto: str, timeout = 30):
+        """
+        Executa um script JavaScript no browser e retorna o resultado.
+        :param script: Código JavaScript a ser executado.
+        :return: Retorno do script executado.
+        """
+        try:
+        # Executa o script JavaScript no navegador
+            WebDriverWait(self.driver, timeout).until(
+            lambda driver: driver.execute_script('return document.querySelector("transfer-popup")') is not None
+        )
+            # sleep(0.5)
+
+            input_element = self.driver.execute_script(script)
+        # Verifica se o campo de entrada foi encontrado
+            if input_element:
+                print("Campo encontrado! Enviando texto...")
+                try:
+                    input_element.click()
+                    input_element.send_keys(texto)  # Envia o texto para o campo
+                except:
+                    input_element.click()
+                    pass
+            else:
+                print("Erro: Input não encontrado!")
+
+        except TimeoutException:
+            print("Erro, elemento não apareceu dentro do tempo limite")
+        except Exception as e:
+            print(f"Erro ao executar o script: {e}")
+
+
     def reason_code_auto(self):
         #Label da ILPN
+        auto_click = AutoClick(self.driver)
         inventory_container_path = "/html/body/app-root/ion-app/div/ion-split-pane/ion-router-outlet/inventory-grid/dm-list-layout/div/div/div[2]/dm-filter/div[2]/div/div[2]/div[2]/text-field-filter/div/ion-row/div/div/ion-input/input"
         checkbox = "//*[@id='main']/inventory-grid/dm-list-layout/div/div/div[3]/div[2]/div[1]/ion-content/grid-view/div/ngx-datatable/div/datatable-body/datatable-selection/datatable-scroller/datatable-row-wrapper[1]/datatable-body-row/div[1]/datatable-body-cell/div/label/input"
         more_options = "//*[@id='main']/inventory-grid/dm-list-layout/div/div/div[3]/div[2]/footer-actions/ion-grid/ion-row/ion-col[3]/div/div/more-actions/ion-button"
         reidentify_item = "//*[@id='mat-menu-panel-2']/div/div[2]/button"
-        source_da_ilpn = "//*[@id='ion-overlay-12']/transfer-popup/ion-content/ion-row[2]/div/div/ion-row[2]/ion-col[1]/div/ion-row/ion-col/div/ion-row"
-        copiar_sku = copiar_sku = "//span[@data-component-id]"
-
-        campo_sku_rdyi = "//*[@id='ion-overlay-6']/transfer-popup/ion-content/ion-row[2]/div/div/ion-row[2]/ion-col[2]/div/ion-row/ion-col/div/form/ion-row/ion-col/div/ion-row[1]/ion-input"
-        popup_reidentify = "//*[@id='ion-overlay-12']//ion-backdrop"
-        auto_click = AutoClick(self.driver)
-        aguardando = Automation(self.driver)
+        copiar_sku =  "005303028"
+        lupa = "/html/body/app-root/ion-app/ion-modal/transfer-popup/ion-content/ion-row[2]/div/div/ion-row[2]/ion-col[2]/div/ion-row/ion-col/div/form/ion-row/ion-col/div/ion-row[1]/div/button"
+        checkbox_sku = "/html/body/app-root/ion-app/ion-modal[2]/lookup-dialogue/modal-container/div/div/modal-content/ion-row[3]/grid-view/div/ngx-datatable/div/datatable-body/datatable-selection/datatable-scroller/datatable-row-wrapper/datatable-body-row/div[1]/datatable-body-cell/div/input"
+        submit_sku = "/html/body/app-root/ion-app/ion-modal[2]/lookup-dialogue/modal-container/div/modal-footer/div/div[2]/ion-button"
+        script_input_item_Name = '''return document.querySelector("transfer-popup").querySelectorAll("input")[0];
+        '''
+        script_input_inventory_type = """return document.querySelector('#\\31 401');"""
+        reasoncode_field = "/html/body/app-root/ion-app/ion-modal/transfer-popup/ion-content/ion-row[2]/div/div/ion-row[2]/ion-col[2]/div/ion-row/ion-col/div/form/ion-row/ion-col/div/ion-row[2]/ion-col[1]/autocomplete/div/ion-input/input"
+        ref1 = "/html/body/app-root/ion-app/ion-modal/transfer-popup/ion-content/ion-row[2]/div/div/ion-row[2]/ion-col[2]/div/ion-row/ion-col/div/form/ion-row/ion-col/div/ion-row[2]/ion-col[2]/input"
+        ref2 = "/html/body/app-root/ion-app/ion-modal/transfer-popup/ion-content/ion-row[2]/div/div/ion-row[2]/ion-col[2]/div/ion-row/ion-col/div/form/ion-row/ion-col/div/ion-row[2]/ion-col[3]/input"
+        attribute1 = "/html/body/app-root/ion-app/ion-modal/transfer-popup/ion-content/ion-row[2]/div/div/ion-row[2]/ion-col[2]/div/ion-row/ion-col/div/form/ion-row/ion-col/div/ion-row[2]/ion-col[3]/ion-item[1]/input"
+        inventory_type = "/html/body/app-root/ion-app/ion-modal/transfer-popup/ion-content/ion-row[2]/div/div/ion-row[2]/ion-col[2]/div/ion-row/ion-col/div/form/ion-row/ion-col/div/ion-row[2]/ion-col[2]/popup-dropdown[1]/ion-label/div"
+        inv_0014 = "/html/body/app-root/ion-app/ion-popover/generic-dropdown/ion-list/ion-item[1]/ion-label/div"
+        inv_1401 = "/html/body/app-root/ion-app/ion-popover/generic-dropdown/ion-list/ion-item[2]/ion-label/div"
+        product_status = "/html/body/app-root/ion-app/ion-modal/transfer-popup/ion-content/ion-row[2]/div/div/ion-row[2]/ion-col[2]/div/ion-row/ion-col/div/form/ion-row/ion-col/div/ion-row[2]/ion-col[2]/popup-dropdown[2]/ion-label/div"
+        product_status_021 = "/html/body/app-root/ion-app/ion-popover/generic-dropdown/ion-list/ion-item[8]/ion-label/div"
+        confir_reidentify_item = "/html/body/app-root/ion-app/ion-modal/transfer-popup/ion-footer/button[1]"
         #Criar Looping para ler as ILPN's
         auto_click.click_elemento(inventory_container_path, 30)
-        auto_click.enviar_keys(inventory_container_path, "Q1144789",30)
+        auto_click.enviar_keys(inventory_container_path, "Q1232393",30)
         auto_click.pressionar_enter(inventory_container_path, 10)
-        # campo_ilpn = self.driver.find_element(By.XPATH,inventory_container_path)
         auto_click.click_elemento(checkbox, 10)
-        sleep(1)
         auto_click.click_elemento(more_options, 10)
         auto_click.click_elemento(reidentify_item, 10)
-        auto_click.click_elemento(copiar_sku, 30)
-        input("Verificando")
-        # campo_ilpn.clear()     
+        self.popup_please_wait
+        self.executar_script(script_input_item_Name,copiar_sku)
+        auto_click.click_elemento(lupa, 30)
+        auto_click.click_elemento(checkbox_sku, 30)
+        auto_click.click_elemento(submit_sku, 30)
+        auto_click.click_elemento(reasoncode_field, 30)
+        auto_click.enviar_keys(reasoncode_field, "T3", 30)
+        auto_click.pressionar_enter(reasoncode_field, 30)
+        auto_click.scroll_to_element(ref1)
+        auto_click.click_elemento(ref1, 30)
+        auto_click.enviar_keys(ref1, "Lucas Inspetor", 30)
+        # auto_click.scroll_to_element(ref2)
+        auto_click.enviar_keys(ref2, "NF: 17488789", 30)
+        auto_click.enviar_keys(attribute1, "BOA", 30)
+        auto_click.click_elemento(inventory_type, 30)
+        auto_click._encontrar_elemento(inventory_type, 30, EC.visibility_of_element_located)
+        menu_item = WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.XPATH, inv_1401)))
+        auto_click.click_elemento(inv_1401, 30)
+        auto_click.scroll_to_element(product_status)
+        auto_click.click_elemento(product_status, 30)
+        auto_click.scroll_to_element(product_status_021)
+        auto_click.click_elemento(product_status_021, 30)
+        auto_click.click_elemento(confir_reidentify_item, 30)
+        auto_click.clear_field(inventory_container_path, 30)
+             
 
     def close_driver(self):
         if self.driver:
